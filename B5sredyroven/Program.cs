19d1de8a94f85604c648944651911f80162c1b1c
Admin <Admin@DESKTOP-UGP0C2G>
2024-10-10 10:42:02 +0200

    Добавьте файлы проекта.

---
+++ B5sredyroven/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите значение e: ");
+        double e = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите значение t: ");
+        double t = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите значение y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        double d = 7.8 * (Math.Pow(a, 2)) + 3.52 * Math.Sqrt(t) / Math.Log(Math.Sqrt(a) + 2 * Math.Sqrt(y) + Math.Pow(e, y));
+        Console.WriteLine($"Значение D равно {d}.");
+    }
+}
